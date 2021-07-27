6b3850ba5d811676434996d4a97912a73d5b81e8
Janer Gonzalez <janer369@gmail.com>
2021-07-26 21:21:32 -0500

    ultima version

---
+++ src/Pages/Home2.js
@@ -0,0 +1,19 @@
+import React, {Component, useState} from 'react';
+
+
+
+class Home2 extends Component{
+
+    constructor() {
+        this.state = {
+            data: [],
+            hasError: false,
+            loading: false
+        }
+
+
+
+
+    
+    }
+}
